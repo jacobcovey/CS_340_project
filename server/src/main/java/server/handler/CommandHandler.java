a3144534072b8a14ab4b55d9fc2c3cc7d3a6a2df
Spencer Wood <spencercwood@gmail.com>
2017-05-15 12:39:41 -0600

    Add working server

---
+++ server/src/main/java/server/handler/CommandHandler.java
@@ -0,0 +1,49 @@
+package server.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+
+public class CommandHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        try {
+            if (!exchange.getRequestMethod().toLowerCase().equals("get")) {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+                exchange.getResponseBody().close();
+                return;
+            }
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+            OutputStream respBody = exchange.getResponseBody();
+            writeString("It worked!", respBody);
+            respBody.close();
+        } catch (IOException e) {
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, 0);
+            exchange.getResponseBody().close();
+            e.printStackTrace(); }
+    }
+
+    private static String readString(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        InputStreamReader sr = new InputStreamReader(is);
+        char[] buf = new char[1024];
+        int len;
+        while ((len = sr.read(buf)) > 0) {
+            sb.append(buf, 0, len);
+        }
+        return sb.toString();
+    }
+
+    private static void writeString(String str, OutputStream os) throws IOException {
+        OutputStreamWriter sw = new OutputStreamWriter(os);
+        sw.write(str);
+        sw.flush();
+    }
+}
--- server/src/main/java/server/main/ServerCommunicator.java
+++ server/src/main/java/server/main/ServerCommunicator.java
@@ -1,4 +1,31 @@
 package server.main;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import server.handler.CommandHandler;
+
 public class ServerCommunicator {
+
+    private HttpServer server;
+    private static final int MAX_WAITING_CONNECTIONS = 12;
+
+    private void run(int port) {
+        try {
+            server = HttpServer.create(new InetSocketAddress(port), MAX_WAITING_CONNECTIONS);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        server.setExecutor(null);
+        server.createContext("/executecommand", new CommandHandler());
+        server.start();
+        System.out.format("Server running at: http://localhost:%d\n", port);
+    }
+
+    public static void main(String[] args) {
+        new ServerCommunicator().run(3000);
+    }
 }
