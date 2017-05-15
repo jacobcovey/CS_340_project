5dbb6acc1fb197e187847c28ff6e9260a68e1a87
Bill Richards <bill.richards@tractusonline.com>
2017-05-15 12:42:31 -0600

    built out shells for commands, need to implement data type, and execute method

---
+++ app/src/main/java/com/example/jacobcovey/commands/GameJoinedCommand.java
@@ -0,0 +1,17 @@
+package com.example.jacobcovey.commands;
+
+import shared.interfaces.iCommand;
+
+/**
+ * Created by billrichards on 5/15/17.
+ */
+
+public class GameJoinedCommand implements iCommand {
+
+    String data;
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ app/src/main/java/com/example/jacobcovey/commands/UpdateGameListCommand.java
@@ -0,0 +1,19 @@
+package com.example.jacobcovey.commands;
+
+import java.util.List;
+
+import shared.interfaces.iCommand;
+
+/**
+ * Created by billrichards on 5/15/17.
+ */
+
+public class UpdateGameListCommand implements iCommand {
+
+    List<String> data;
+
+    @Override
+    public void execute() {
+
+    }
+}
