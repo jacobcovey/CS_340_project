1c59241c25db96ebd225fb8c3b8f64962027bfac
dylanmoh <dylanmoh94@gmail.com>
2017-05-31 12:12:04 -0600

    added initializeCities method to GameStart command

--- server/src/main/java/server/model/ServerModelRoot.java
+++ server/src/main/java/server/model/ServerModelRoot.java
@@ -25,42 +25,6 @@ public class ServerModelRoot {
     }
 
     private ServerModelRoot() {
-        cities.add(new City("Vancouver"));
-        cities.add(new City("Calgary"));
-        cities.add(new City("Winnipeg"));
-        cities.add(new City("Sault St. Marie"));
-        cities.add(new City("Montreal"));
-        cities.add(new City("Seattle"));
-        cities.add(new City("Helena"));
-        cities.add(new City("Duluth"));
-        cities.add(new City("Toronto"));
-        cities.add(new City("Boston"));
-        cities.add(new City("New York"));
-        cities.add(new City("Pittsburg"));
-        cities.add(new City("Chicago"));
-        cities.add(new City("Omaha"));
-        cities.add(new City("Denver"));
-        cities.add(new City("Kansas City"));
-        cities.add(new City("Saint Louis"));
-        cities.add(new City("Nashville"));
-        cities.add(new City("Raleigh"));
-        cities.add(new City("Washington"));
-        cities.add(new City("Charleston"));
-        cities.add(new City("Atlanta"));
-        cities.add(new City("Oklahoma City"));
-        cities.add(new City("Little Rock"));
-        cities.add(new City("Miami"));
-        cities.add(new City("New Orleans"));
-        cities.add(new City("Houston"));
-        cities.add(new City("Dallas"));
-        cities.add(new City("El Paso"));
-        cities.add(new City("Santa Fe"));
-        cities.add(new City("Phoenix"));
-        cities.add(new City("Los Angeles"));
-        cities.add(new City("Las Vegas"));
-        cities.add(new City("Salt Lake City"));
-        cities.add(new City("Portland"));
-        cities.add(new City("San Francisco"));
     }
 
     private Set<User> users = new HashSet<>();
@@ -119,5 +83,44 @@ public class ServerModelRoot {
             }
         }
         return null;
+    }
+
+    public void initializeCities () {
+        cities.add(new City("Vancouver"));
+        cities.add(new City("Calgary"));
+        cities.add(new City("Winnipeg"));
+        cities.add(new City("Sault St. Marie"));
+        cities.add(new City("Montreal"));
+        cities.add(new City("Seattle"));
+        cities.add(new City("Helena"));
+        cities.add(new City("Duluth"));
+        cities.add(new City("Toronto"));
+        cities.add(new City("Boston"));
+        cities.add(new City("New York"));
+        cities.add(new City("Pittsburg"));
+        cities.add(new City("Chicago"));
+        cities.add(new City("Omaha"));
+        cities.add(new City("Denver"));
+        cities.add(new City("Kansas City"));
+        cities.add(new City("Saint Louis"));
+        cities.add(new City("Nashville"));
+        cities.add(new City("Raleigh"));
+        cities.add(new City("Washington"));
+        cities.add(new City("Charleston"));
+        cities.add(new City("Atlanta"));
+        cities.add(new City("Oklahoma City"));
+        cities.add(new City("Little Rock"));
+        cities.add(new City("Miami"));
+        cities.add(new City("New Orleans"));
+        cities.add(new City("Houston"));
+        cities.add(new City("Dallas"));
+        cities.add(new City("El Paso"));
+        cities.add(new City("Santa Fe"));
+        cities.add(new City("Phoenix"));
+        cities.add(new City("Los Angeles"));
+        cities.add(new City("Las Vegas"));
+        cities.add(new City("Salt Lake City"));
+        cities.add(new City("Portland"));
+        cities.add(new City("San Francisco"));
     }
 }
