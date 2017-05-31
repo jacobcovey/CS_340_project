bf894b07302f641a8b5e019a8f380f7eb55d3592
Bill Richards <bill.richards@tractusonline.com>
2017-05-30 19:45:41 -0600

    added Icons to map view

--- app/src/main/java/com/example/jacobcovey/Activities/GameBoardActivity.java
+++ app/src/main/java/com/example/jacobcovey/Activities/GameBoardActivity.java
@@ -1,10 +1,15 @@
 package com.example.jacobcovey.Activities;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.jacobcovey.Views.GameBoardView;
 import com.example.jacobcovey.Views.GameInfoView;
 import com.example.jacobcovey.Views.TrainCardDrawerView;
+import com.example.jacobcovey.constants.Icons;
+import com.example.jacobcovey.ticket_to_ride.R;
 
 /**
  * Created by jacobcovey on 5/19/17.
@@ -13,6 +18,33 @@ import com.example.jacobcovey.Views.TrainCardDrawerView;
 public class GameBoardActivity extends SingleFragmentActivity implements TrainCardDrawerView.TrainCardDrawerContainer, GameInfoView.GameInfoDrawerContainer {
 
     private GameBoardView gameBoard;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.gameboardmenu, menu);
+        menu.findItem(R.id.action_chat).setIcon(Icons.chatIcon);
+        menu.findItem(R.id.action_history).setIcon(Icons.historyIcon);
+        menu.findItem(R.id.action_info).setIcon(Icons.gameInfoIcon);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch(item.getItemId()) {
+            case R.id.action_chat:
+                gameBoard.presentChatDrawer();
+                break;
+            case R.id.action_history:
+                gameBoard.presentHistoryDrawer();
+                break;
+            case R.id.action_info:
+                gameBoard.presentGameInfoDrawer();
+                break;
+            default:
+        }
+        return true;
+    }
 
     @Override
     protected Fragment createFragment() {
--- app/src/main/java/com/example/jacobcovey/Activities/LoginActivity.java
+++ app/src/main/java/com/example/jacobcovey/Activities/LoginActivity.java
@@ -1,12 +1,26 @@
 package com.example.jacobcovey.Activities;
 
+import android.os.Bundle;
+
 import com.example.jacobcovey.Views.LoginView;
+import com.example.jacobcovey.constants.Icons;
+import com.joanzapata.iconify.Iconify;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 public class LoginActivity extends SingleFragmentActivity {
 
     @Override
     protected android.support.v4.app.Fragment createFragment() {
         return new LoginView();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        Iconify.with(new FontAwesomeModule());
+        new Icons(this);
+
     }
 
 }
