246265eca0839b6276e06936e3ebe13357c52056
Jacob Covey <jacobcovey@gmail.com>
2017-05-31 23:17:06 -0600

    Good stuff

--- app/src/main/java/com/example/jacobcovey/Presenters/IGameInfoPresenter.java
+++ app/src/main/java/com/example/jacobcovey/Presenters/IGameInfoPresenter.java
@@ -1,10 +1,12 @@
 package com.example.jacobcovey.Presenters;
 
 import com.example.jacobcovey.Views.IGameInfoView;
+import com.example.jacobcovey.model.GameInfo;
 
 import java.util.Set;
 
 import shared.classes.DestinationCard;
+import shared.classes.Player;
 
 /**
  * Created by jacobcovey on 5/24/17.
@@ -17,5 +19,9 @@ public interface IGameInfoPresenter {
     Set<DestinationCard> getRoutes();
 
     public void removeObserver();
+
+    GameInfo getGameInfo();
+
+    Player getCurrentPlayer();
 
 }
